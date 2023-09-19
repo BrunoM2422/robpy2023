be1afe0b22d768328112b843f116778009948732
BRUNO LENITTA MACHADO <bruno.lm6@puccampinas.edu.br>
2023-09-19 16:23:41 -0300

    implementada a função plot_vetor3

--- RobPy.py
+++ RobPy.py
@@ -85,17 +85,22 @@ def produto_vetorial(v1: np.ndarray, v2: np.ndarray) -> np.ndarray:
     :param v2: vetor (np.ndarray) coluna de 3 elementos
     :return: vetor (np.ndarray) coluna de 3 elementos com o resultado de v1 x v2
     """
-    pass
+    checa_vetor3(v1)
+    checa_vetor3(v2)
+
+    return np.asarray([[v1[1][0]*v2[2][0] - v1[2][0]*v2[1][0]],
+                        [v1[2][0]*v2[0][0] - v1[0][0]*v2[2][0]],
+                         [v1[0][0]*v2[1][0] - v1[1][0]*v2[0][0]]])
 
 
 # Parte 2
 
 
 def plota_vetor3(v: np.ndarray,
-                 ax: plot.Axes,
                  *args,
                  vo: np.ndarray = np.zeros([3, 1]),
-                 zdir='z', **kwargs) -> list:
+                 color='b',
+                **kwargs) -> list:
     """
     Utiliza o pacote matplotlib.plotpy para plotar um vetor em um diagrama 3D. É necessário utilizar eixos criados com o
     comando matplotlib.plotly.axis(projection='3d').
@@ -109,8 +114,20 @@ def plota_vetor3(v: np.ndarray,
     :param kwargs: parâmetros padrão do plot.
     :return: lista de elementos de linha do vetor plotado.
     """
-    pass
+    a = plot.plot([0, v[0][0], v[0][0] + vo[0][0]],
+              [0, v[1][0], v[1][0] + vo[1][0]],
+              [0, v[2][0], v[2][0] + vo[2][0]],
+              linewidth='4',
+              color=color)
 
+    b = plot.plot(v[0][0] + vo[0][0],
+              v[1][0] + vo[1][0],
+              v[2][0] + vo[2][0],
+              marker='>',
+              markersize=15,
+              color=color)
+
+    return [a,b]
 
 def matriz_rotacao_x(theta: float) -> np.ndarray:
     """
--- testes.py
+++ testes.py
@@ -1,18 +1,17 @@
 # Arquivo de testes
 
+import numpy as np
 import RobPy as rp
+import matplotlib.pyplot as plot
+from mpl_toolkits.mplot3d.axes3d import Axes3D
 
-a = rp.cria_vetor3([1, 2, 3])
-b = rp.cria_vetor3([4, 5, 6])
+a = np.asarray([[1,2,3]]) .T
+b = np.asarray([[4,5,6]]) .T
 
-print(rp.produto_escalar(a, b))
-print(rp.norma_vetor(a))
+fig: plot.Figure = plot.figure()
+ax = Axes3D(fig)
+fig.add_axes(ax)
+rp.plota_vetor3(a, color='m')
 
-
-
-
-
-
-
-
+plot.show()
 
